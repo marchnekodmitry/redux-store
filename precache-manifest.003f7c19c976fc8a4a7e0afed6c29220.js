e4556910c6e54fd51ec741912e910b3f189e473b
nogandalfgrey <no.gandalf.grey@gmail.com>
2019-07-21 15:24:56 +0300

    Updates

---
+++ precache-manifest.003f7c19c976fc8a4a7e0afed6c29220.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a269da0d2754d6ca1ecd64173c81245c",
+    "url": "/redux-store/index.html"
+  },
+  {
+    "revision": "e85c371d2e2d05413210",
+    "url": "/redux-store/static/css/main.5cf67ee3.chunk.css"
+  },
+  {
+    "revision": "5ec4de0ad9b6a456a441",
+    "url": "/redux-store/static/js/2.e81692cb.chunk.js"
+  },
+  {
+    "revision": "e85c371d2e2d05413210",
+    "url": "/redux-store/static/js/main.c162d0cc.chunk.js"
+  },
+  {
+    "revision": "1fa2efe3557145ca6288",
+    "url": "/redux-store/static/js/runtime~main.42348e37.js"
+  }
+]);
